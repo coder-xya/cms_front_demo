26e119a565ece75a87c6b7463e98b70a826a17c6
coder-xya <coderyuqian@gmail.com>
2025-07-23 18:08:25 +0800

    优化axios@1.11.0版本封装

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,92 +1,112 @@
-import axios from 'axios';
-import type { AxiosInstance } from 'axios';
+import axios, {
+  type AxiosInstance,
+  type InternalAxiosRequestConfig,
+  type AxiosResponse,
+  AxiosHeaders,
+} from 'axios';
 import type { XYARequestConfig } from './type';
-
-// 拦截器: 蒙版Loading/token/修改配置
-
-/**
- * 两个难点:
- *  1.拦截器进行精细控制
- *    > 全局拦截器
- *    > 实例拦截器
- *    > 单次请求拦截器
- *
- *  2.响应结果的类型处理(泛型)
- */
 
 class XYARequest {
   instance: AxiosInstance;
 
-  // request实例 => axios的实例
   constructor(config: XYARequestConfig) {
     this.instance = axios.create(config);
 
-    // 每个instance实例都添加拦截器
+    // 全局请求拦截器
     this.instance.interceptors.request.use(
-      (config) => {
-        // loading/token
+      (config: InternalAxiosRequestConfig) => {
+        // 确保 headers 存在
+        config.headers = config.headers || new AxiosHeaders();
+        // 可添加全局逻辑，如 token
+        // config.headers.set('Authorization', 'Bearer token');
         return config;
       },
-      (err) => {
-        return err;
-      },
-    );
-    this.instance.interceptors.response.use(
-      (res) => {
-        return res.data;
-      },
-      (err) => {
-        return err;
+      (err: any) => {
+        return Promise.reject(err);
       },
     );
 
-    //TODO 类型待完善
-    // 针对特定的XYARequest实例添加拦截器
-    this.instance.interceptors.request.use(
-      config.interceptors?.requestSuccessFn,
-      config.interceptors?.requestFailureFn,
-    );
+    // 全局响应拦截器
     this.instance.interceptors.response.use(
-      config.interceptors?.responseSuccessFn,
-      config.interceptors?.responseFailureFn,
+      (res: AxiosResponse) => {
+        return res.data; // 直接返回数据
+      },
+      (err: any) => {
+        return Promise.reject(err);
+      },
     );
+
+    // 实例特定的拦截器
+    if (config.interceptors) {
+      // 请求拦截器
+      this.instance.interceptors.request.use(
+        config.interceptors.requestSuccessFn as
+          | ((
+              value: InternalAxiosRequestConfig,
+            ) =>
+              | InternalAxiosRequestConfig
+              | Promise<InternalAxiosRequestConfig>)
+          | null,
+        config.interceptors.requestFailureFn,
+      );
+
+      // 响应拦截器 - 处理类型转换
+      this.instance.interceptors.response.use((res: AxiosResponse) => {
+        // 如果实例有响应拦截器，传递响应数据
+        if (config.interceptors?.responseSuccessFn) {
+          return config.interceptors.responseSuccessFn(res.data);
+        }
+        return res.data;
+      }, config.interceptors.responseFailureFn);
+    }
   }
 
-  // 封装网络请求的方法
-  // T => IHomeData
-  request<T = any>(config: XYARequestConfig<T>) {
-    // 单次请求的成功拦截处理
+  // 封装网络请求的核心方法
+  async request<T = any>(config: XYARequestConfig<T>): Promise<T> {
+    // 单次请求的请求拦截器处理
+    let finalConfig: InternalAxiosRequestConfig =
+      config as unknown as InternalAxiosRequestConfig;
+
     if (config.interceptors?.requestSuccessFn) {
-      config = config.interceptors.requestSuccessFn(config);
+      finalConfig = await config.interceptors.requestSuccessFn(finalConfig);
     }
 
-    // 返回Promise
-    return new Promise<T>((resolve, reject) => {
-      this.instance
-        .request<any, T>(config)
-        .then((res) => {
-          // 单词响应的成功拦截处理
-          if (config.interceptors?.responseSuccessFn) {
-            res = config.interceptors.responseSuccessFn(res);
-          }
-          resolve(res);
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
+    try {
+      // 发送请求
+      const response = await this.instance.request<T>(finalConfig);
+
+      // 处理响应数据
+      let result: T = response as T;
+
+      // 单次请求的响应拦截器处理
+      if (config.interceptors?.responseSuccessFn) {
+        result = await config.interceptors.responseSuccessFn(result);
+      }
+
+      return result;
+    } catch (err) {
+      // 单次请求的错误拦截器处理
+      if (config.interceptors?.responseFailureFn) {
+        throw config.interceptors.responseFailureFn(err);
+      }
+      throw err;
+    }
   }
 
-  get<T = any>(config: XYARequestConfig<T>) {
+  // 简化方法
+  get<T = any>(config: XYARequestConfig<T>): Promise<T> {
     return this.request({ ...config, method: 'GET' });
   }
-  post<T = any>(config: XYARequestConfig<T>) {
+
+  post<T = any>(config: XYARequestConfig<T>): Promise<T> {
     return this.request({ ...config, method: 'POST' });
   }
-  delete<T = any>(config: XYARequestConfig<T>) {
+
+  delete<T = any>(config: XYARequestConfig<T>): Promise<T> {
     return this.request({ ...config, method: 'DELETE' });
   }
-  patch<T = any>(config: XYARequestConfig<T>) {
+
+  patch<T = any>(config: XYARequestConfig<T>): Promise<T> {
     return this.request({ ...config, method: 'PATCH' });
   }
 }
--- src/service/request/type.ts
+++ src/service/request/type.ts
@@ -1,14 +1,17 @@
-import type { AxiosRequestConfig, AxiosResponse } from 'axios';
+import type { AxiosRequestConfig, InternalAxiosRequestConfig } from 'axios';
 
-// 针对AxiosRequestConfig配置进行扩展
-export interface XYAInterceptors<T = AxiosResponse> {
-  requestSuccessFn?: (config: AxiosRequestConfig) => AxiosRequestConfig;
+export interface XYAInterceptors<T = any> {
+  requestSuccessFn?: (
+    config: InternalAxiosRequestConfig,
+  ) => InternalAxiosRequestConfig | Promise<InternalAxiosRequestConfig>;
+
   requestFailureFn?: (err: any) => any;
-  responseSuccessFn?: (res: T) => T;
+
+  responseSuccessFn?: (res: T) => T | Promise<T>;
+
   responseFailureFn?: (err: any) => any;
 }
 
-export interface XYARequestConfig<T = AxiosResponse>
-  extends AxiosRequestConfig {
+export interface XYARequestConfig<T = any> extends AxiosRequestConfig {
   interceptors?: XYAInterceptors<T>;
 }
