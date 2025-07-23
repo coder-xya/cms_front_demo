b41088bbdaad587e0bf4600a09eb873b566ec1d6
coder-xya <coderyuqian@gmail.com>
2025-07-23 09:38:17 +0800

    prettier eslint配置

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const info = {
+  name: 'why',
+};
+
+createApp(App).mount('#app');
